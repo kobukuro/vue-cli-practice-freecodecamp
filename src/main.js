47038692b52eece31e3d90b697b4884eda8cda78
Peter Chen <peter.kt.chen@mail.foxconn.com>
2021-02-01 16:25:13 +0800

    import bootstrap and bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+//用下面指令安裝bootstrap以及bootstrap-vue
+//npm install bootstrap bootstrap-vue
